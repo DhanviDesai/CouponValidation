93f9f974549cf04011d3b172dc004bbc7b559e0b
Dhanvi Desai <dhanvidesai99@gmail.com>
2022-02-16 22:08:58 +0530

    deploy to heroku changes - 4

--- flask_server/app.py
+++ flask_server/app.py
@@ -4,15 +4,15 @@ import flask_server.routes as routes
 import flask_server.config as config
 from flask_cors import CORS,cross_origin
 
-server = Flask(__name__,static_folder="react_app/build")
-CORS(server)
+app = Flask(__name__,static_folder="react_app/build")
+CORS(app)
 
 for blueprint in vars(routes).values():
     if isinstance(blueprint,Blueprint):
-        server.register_blueprint(blueprint)
+        app.register_blueprint(blueprint)
 
-@server.route('/')
+@app.route('/')
 def serve():
-    return send_from_directory(server.static_folder,"index.html")
+    return send_from_directory(app.static_folder,"index.html")
 
-server.run(port=config.PORT)
+# server.run(port=config.PORT)
