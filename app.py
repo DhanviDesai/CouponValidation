9ce696315d20411c9f043c2e8030faca73e20bb6
Dhanvi Desai <dhanvidesai99@gmail.com>
2022-02-16 21:51:22 +0530

    deployment changes initialized

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask.blueprints import Blueprint
 import flask_server.routes as routes
 import flask_server.config as config
+from flask_cors import CORS,cross_origin
 
-server = Flask(__name__)
+server = Flask(__name__,static_folder="react_app/build")
+CORS(server)
 
 for blueprint in vars(routes).values():
     if isinstance(blueprint,Blueprint):
         server.register_blueprint(blueprint)
+
+@server.route('/')
+def serve():
+    return send_from_directory(server.static_folder,"index.html")
 
 server.run(port=config.PORT)
